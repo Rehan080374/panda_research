7419c6e0ba242ddd1d182290c6e0a15a869b54b0
rehan080374 <rehan.080374@cmail.com>
2023-01-12 13:12:28 -0800

    camera working

---
+++ camera_opencv.py
@@ -0,0 +1,103 @@
+'''
+///////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2018, STEREOLABS.
+//
+// All rights reserved.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+///////////////////////////////////////////////////////////////////////////
+
+/*****************************************************************************************
+ ** This sample demonstrates how to capture stereo images and calibration parameters    **
+ ** from the ZED camera with OpenCV without using the ZED SDK.                          **
+ *****************************************************************************************/
+'''
+
+import numpy as np
+import os
+import configparser
+import sys
+import cv2
+import wget
+import mediapipe as mp
+
+
+
+class Resolution :
+     width = 1280
+     height = 720
+
+def main() :
+    serial_number = 19116715
+    mp_drawing = mp.solutions.drawing_utils
+    mp_drawing_styles = mp.solutions.drawing_styles
+    mp_hands = mp.solutions.hands
+    # if len(sys.argv) == 1 :
+    #     print('Please provide ZED serial number')
+    #     exit(1)
+
+    # Open the ZED camera
+    cap = cv2.VideoCapture(0)
+    if cap.isOpened() == 0:
+        exit(-1)
+
+    image_size = Resolution()
+    image_size.width = 1280
+    image_size.height = 720
+
+    # Set the video resolution to HD720
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, image_size.width*2)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, image_size.height)
+
+    
+    with mp_hands.Hands(
+    model_complexity=0,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5) as hands:
+    
+        while cap.isOpened():
+            # Get a new frame from camera
+            retval, frame = cap.read()
+            # Extract left and right images from side-by-side
+            left_right_image = np.split(frame, 2, axis=1)
+           
+            image=left_right_image[0]
+            image.flags.writeable = False
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            results = hands.process(image)
+
+            # Draw the hand annotations on the image.
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            
+            if results.multi_hand_landmarks:
+                # print(i)
+                # i+=1
+                for hand_landmarks in results.multi_hand_landmarks:
+                    mp_drawing.draw_landmarks(image,hand_landmarks,mp_hands.HAND_CONNECTIONS,mp_drawing_styles.get_default_hand_landmarks_style(),mp_drawing_styles.get_default_hand_connections_style())
+                   
+                        
+            # Flip the image horizontally for a selfie-view display.
+            cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
+            #cv2.imshow("left RECT", left_rect)
+
+            #cv2.imshow("right RECT", right_rect)
+            if cv2.waitKey(30) >= 0 :
+                break
+
+    exit(0)
+
+if __name__ == "__main__":
+    main()
