8255a0ae8bf32fbb102c623123ab1ec418bc1359
rehan080374 <rehan.080374@cmail.com>
2023-03-21 16:08:36 -0700

    testing march21

--- camera applications/camera_opencv.py
+++ camera applications/camera_opencv.py
@@ -31,8 +31,18 @@ import configparser
 import sys
 import cv2
 import mediapipe as mp
+# from mediapipe.tasks.python import vision
+# from mediapipe.tasks import python
+# from mediapipe_model_maker import gesture_recognizer
 
-
+BaseOptions = mp.tasks.BaseOptions
+GestureRecognizer = mp.tasks.vision.GestureRecognizer
+GestureRecognizerOptions = mp.tasks.vision.GestureRecognizerOptions
+GestureRecognizerResult = mp.tasks.vision.GestureRecognizerResult
+VisionRunningMode = mp.tasks.vision.RunningMode
+# Create a gesture recognizer instance with the live stream mode:
+def print_result(result: GestureRecognizerResult, output_image: mp.Image, timestamp_ms: int):
+    print('gesture recognition result: {}'.format(result))
 
 class Resolution :
      width = 1280
@@ -52,45 +62,85 @@ def main() :
     if cap.isOpened() == 0:
         exit(-1)
 
-    image_size = Resolution()
-    image_size.width = 1280
-    image_size.height = 720
+    # image_size = Resolution()
+    # image_size.width = 1280
+    # image_size.height = 720
 
     # Set the video resolution to HD720
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, image_size.width*2)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, image_size.height)
+    # cap.set(cv2.CAP_PROP_FRAME_WIDTH, image_size.width)
+    # cap.set(cv2.CAP_PROP_FRAME_HEIGHT, image_size.height)
+    # STEP 2: Create an GestureRecognizer object.
+    
+    options = GestureRecognizerOptions(
+    base_options=BaseOptions(model_asset_path='/home/panda/test_ws/src/panda_repository/panda_research/camera applications/gesture_recognizer.task'),
+    running_mode=VisionRunningMode.IMAGE,
+    )
 
-    
-    with mp_hands.Hands(
-    model_complexity=0,
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as hands:
-    
+
+        
+    # with mp_hands.Hands(
+    # model_complexity=0,
+    # min_detection_confidence=0.5,
+    # min_tracking_confidence=0.5) as hands:
+    with GestureRecognizer.create_from_options(options) as recognizer:
+
         while cap.isOpened():
             # Get a new frame from camera
             retval, frame = cap.read()
             # Extract left and right images from side-by-side
-            #left_right_image = np.split(frame, 2, axis=1)
-           
-            image=frame
-            image.flags.writeable = False
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            results = hands.process(image)
+            left_right_image = np.split(frame, 2, axis=1)
+            image = cv2.cvtColor(left_right_image[0], cv2.COLOR_BGR2RGB)
+            mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
+            # recognizer.recognize_async(mp_image, frame_timestamp_ms)
+            width = image.shape[0]
+            height = image.shape[1]
+            # image=left_right_image[0]
+            
 
+            # image = mp.Image.create_from_file(image)
+            # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) 
+            # image_packet = mp.packet_creator.create_image( image_format=unit., data=image)
+
+            # image.flags.writeable = True
+            # image_frame_packet = mp.packet_creator.create_image_frame(image_format=mp.ImageFormat.SRGB, data=image)
+            # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            results = recognizer.recognize(mp_image)
+            # image = cv2.cvtColor(left_right_image[0], cv2.COLOR_RGB2BGR)
+            # print(results)
+            #  results = hands.process(image)
+            # top_gesture = recognition_result.Gestures[0].Categories[0].catagoryName 
+            hand_landmarks = np.array(results.hand_landmarks)
+            
             # Draw the hand annotations on the image.
-            image.flags.writeable = True
+            # image.flags.writeable = True
             image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
             
-            if results.multi_hand_landmarks:
-                # print(i)
-                # i+=1
-                for hand_landmarks in results.multi_hand_landmarks:
-                    mp_drawing.draw_landmarks(image,hand_landmarks,mp_hands.HAND_CONNECTIONS,mp_drawing_styles.get_default_hand_landmarks_style(),mp_drawing_styles.get_default_hand_connections_style())
-                    print(f'Index finger tip coordinate: (',f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_size.width}, 'f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_size.height})')
-                
-                        
+            if results.handedness:
+                print(results.gestures[0][0].category_name)
+                # print("handedness=",results.handedness[0].display_name)
+            #     # print(i)
+            #     # i+=1
+                # landmarks=[]
+                for i in range(20):
+                    # x=round(hand_landmarks[0][i].x * width)
+                    # y=round(hand_landmarks[0][i].y* height)
+                    # cent=(x,y)
+                    p1=[hand_landmarks[0][i].x * width,hand_landmarks[0][i].y * height]
+                #   print(f'Index finger tip coordinate: ',i,'  (',f'{hand_landmarks[0][i].x * image_size.width}, 'f'{hand_landmarks[0][i].y * image_size.height})')  
+                    cv2.circle(image,p1,radius=3,color=(0,255,255),thickness=10)
+                # for  hand_landmark in results.hand_landmarks:
+                    # mp_drawing.draw_landmarks(image,Landmark,mp_hands.HAND_CONNECTIONS,mp_drawing_styles.get_default_hand_landmarks_style(),mp_drawing_styles.get_default_hand_connections_style())
+            #          mp_drawing.draw_landmarks(image,landmarks,mp_hands.HAND_CONNECTIONS,mp_drawing_styles.get_default_hand_landmarks_style(),mp_drawing_styles.get_default_hand_connections_style())
+                #    for Landmark in  hand_landmarks:     
+                # mp_drawing.draw_landmarks(image,results.hand_world_landmarks, mp_hands.HAND_CONNECTIONS)
+                    # print(f'Index finger tip coordinate: (',f'{landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_size.width}, 'f'{landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_size.height})')
+                    print(x,y )
+                # for landmark in  landmarks:
+                # mp_drawing.draw_landmarks(image,landmarks, mp_hands.HAND_CONNECTIONS)
+
+            cv2.imshow('MediaPipe Hands', image)            
             # Flip the image horizontally for a selfie-view display.
-            cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
+            # cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
             #cv2.imshow("left RECT", left_rect)
 
             #cv2.imshow("right RECT", right_rect)
